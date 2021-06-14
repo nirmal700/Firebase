356b1e36c6d01bb6383f73ebd94d836a51ac2fb8
K NIRMAL KUMAR <k.nirmalkumar2002@gmail.com>
2021-06-15 02:46:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mOutputText,mOutputTex2;
+    Button mButton,pButton,nbutton;
+    EditText mInputText,mInputText2,mId;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton=findViewById(R.id.button);
+        mOutputText=findViewById(R.id.TextView);
+        mInputText=findViewById(R.id.TextField);
+        pButton=findViewById(R.id.button2);
+        mOutputTex2=findViewById(R.id.textView3);
+        mInputText2=findViewById(R.id.EditText2);
+        nbutton=findViewById((R.id.nextbutton));
+        mId=findViewById(R.id.editTextTextPersonName3);
+        mDatabase=FirebaseDatabase.getInstance();
+        mRef=mDatabase.getReference();
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data=mInputText.getText().toString();
+                String age = mInputText2.getText().toString();
+                String id = mId.getText().toString();
+                mRef.child(id).child("Patient Name").setValue(data);
+                mRef.child(id).child("Age").setValue(age);
+                Toast.makeText(MainActivity.this, "Data has been pushed Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+        pButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRef.child("8345").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                        Map<String,Object> data = (Map<String, Object>)snapshot.getValue();
+                        String name = (String) data.get("Patient Name");
+                           mOutputText.setText(name);
+                         String age = (String)data.get("Age");
+                         mOutputTex2.setText(age);
+
+                        Log.d("TAG","On data change : Name :" + data.get("Patient Name"));
+                        Log.d("TAG","On data change : Age :" + data.get("Age"));
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull  DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+        nbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Patient_data.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/firebase/Patient_data.java
@@ -0,0 +1,60 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Patient_data extends AppCompatActivity {
+    EditText mPid,mEmail,mPname,mCnumber,mDob,mId;
+    Button mSave, mDobbutton;
+    RadioGroup mBgroup,mBtype,mGender;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    DatePickerDialog datePickerDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_data);
+        mDatabase=FirebaseDatabase.getInstance();
+        mRef=mDatabase.getReference();
+        mPid=findViewById(R.id.patientid);
+        mEmail=findViewById(R.id.emailid);
+        mPname=findViewById(R.id.patientname);
+        mCnumber=findViewById(R.id.contactid);
+        mDob=findViewById(R.id.dobid);
+        mId=findViewById(R.id.nid);
+        mBgroup=findViewById(R.id.bgid);
+        mBtype=findViewById(R.id.btid);
+        mGender=findViewById((R.id.genderid));
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String mpid,memail,mpname,mcnumber,mdob,mid;
+               mpid=mPid.getText().toString();
+               memail=mEmail.getText().toString();
+               mpname=mPname.getText().toString();
+               mcnumber=mCnumber.getText().toString();
+               mdob=mDob.getText().toString();
+               mid=mId.getText().toString();
+                mRef.child(mpid).child("Patient ID").setValue(mpid);
+                mRef.child(mpid).child("Patient E-mail").setValue(memail);
+                mRef.child(mpid).child("Patient Name").setValue(mpname);
+                mRef.child(mpid).child("Patient Contact Number").setValue(mcnumber)
+                ;mRef.child(mpid).child("Patient DOB").setValue(mdob);
+                mRef.child(mpid).child("Patient I-D Card").setValue(mid);
+                Toast.makeText(Patient_data.this, "The data has been pushed Successfully", Toast.LENGTH_SHORT).show();
+                
+            }
+        });
+    }
+}
